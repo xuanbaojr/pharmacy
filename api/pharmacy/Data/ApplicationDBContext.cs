ce7467abdaf7a0f6b24137987e35e367a9861835
DoanManhDuong <andrew.doan2003@gmail.com>
2024-10-22 09:06:58 +0700

    add login, register, comment api

---
+++ api/pharmacy/Data/ApplicationDBContext.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using pharmacy.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace pharmacy.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
